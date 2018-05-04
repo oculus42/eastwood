ae8f9c78f425fdcc015bf68e045dabb3e0b2d34d
Samuel Rouse <samuel.rouse@gmail.com>
2018-05-03 22:56:04 -0400

    Fix length check bug
    - was preventing other methods from being adopted.

--- bin/eastwood.js
+++ bin/eastwood.js
@@ -12,15 +12,13 @@ if (!module.parent) {
     // Slice out the script from the args
     const origArgs = process.argv.slice(process.argv[0].endsWith('node') ? 2 : 1);
 
-    // Optional default logic if no arguments were provided
-    const myArgs = origArgs.length < 3 ? defaultArgs : origArgs;
+    const myArgs = !origArgs.length ? defaultArgs : origArgs;
     const method = myArgs[0];
 
     // Check if the argument is one of our named methods
     if (methods[method]) {
         // Args arrive as strings, so this might need more work.
         // Arrays and Objects are probably not convenient.
-
         console.log(`Installing ${method}`);
 
         // Execute our method with the rest of the command-line arguments
