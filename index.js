086b670a15fbf4e4cd979ced43e9473e9237ed8c
Samuel Rouse <samuel.rouse@gmail.com>
2018-05-09 01:56:31 -0400

    Update index.js
    Switch to getConfig/runConfig from the old config export.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./src/configs');
+/* eslint no-console:0 */
+
+const getConfig = require('./src/getConfig');
+const runConfig = require('./src/runConfig');
+
+const run = configName => getConfig(configName)
+  .then((config) => {
+    console.log(`Installing ${configName}`);
+    return config;
+  })
+  .then(runConfig)
+  .then(({ data }) => console.log(data), err => console.error(err));
+
+module.exports = run;
